9af109f6bc5bf8ba823dfa90c3007fd242c8f9cb
mmoghal <mmoghal29@gmail.com>
2023-03-25 18:10:54 -0400

    Added all scripts from legacy project

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# work-day-scheduler
-work-day-scheduler
+# Work Day Scheduler
+
+## Description
+
+This is a simple day planner application that allows users to input and save their hourly schedule. The application was built using HTML, CSS, and JavaScript, and relies on the Day.js library to handle date and time formatting.
+
+## Features
+
+The day planner application provides the following features:
+
+.   The current date and time is displayed at the top of the page.
+
+.   Hourly time slots are displayed from 9am to 5pm.
+
+.   Each hourly time slot contains a text input field and a "Save" button.
+
+.   User input is saved to local storage when the "Save" button is clicked.
+
+.   User input is retrieved from local storage and displayed in the corresponding time slot on page load.
+
+.   The background color of each time slot changes based on whether it is in the past, present, or future.
+
+.   The "Clear Schedule" button can be used to clear all user input from local storage and refresh the page.
+
+
+## Table of Contents
+
+.   Description
+
+.   Features
+
+.   Installation
+
+.   Usage
+
+.   Development
+
+.   Credits
+
+.   License
+
+## Installation
+
+To use this day planner application, simply download the source code and open the index.html file in a web browser.
+
+## Usage
+
+Once the day planner application is open in a web browser, simply enter your schedule for each hourly time slot and click the "Save" button. Your schedule will be saved to local storage and displayed on page load.
+
+To clear your entire schedule, click the "Clear Day" button. This will clear all user input from local storage and refresh the page.
+
+
+## Development
+
+This application was developed using HTML, CSS, and JavaScript. 
+
+1. Visit [Deployed Application](https://mmoghal.github.io/Work-Day-Scheduler/) for Work-Day-Scheduler
+
+2. Application Image
+
+![alt Image of the application](https://github.com/mmoghal/work-day-scheduler/blob/main/assets/images/dayplanner.png)
+
+
+## Credits
+
+This day planner application was built by Muhammad Moghal using the following resources:
+
+.   Day.js - A JavaScript library for date and time formatting.
+
+.   Bootstrap - A popular CSS framework for building responsive web applications.
+
+
+## License
+
+This day planner application is licensed under the MIT License.
---
+++ assets/scripts/script.js
@@ -0,0 +1,103 @@
+// Get the current date and time using Moment.js
+
+const currentDate = dayjs().format('dddd D MMM YYYY');
+const currentHour = dayjs().format('h:mm:ss a');
+
+
+// Get references to each hour block on the page
+var hours = [
+  $("#9am"),
+  $("#10am"),
+  $("#11am"),
+  $("#12pm"),
+  $("#13pm"),
+  $("#14pm"),
+  $("#15pm"),
+  $("#16pm"),
+  $("#17pm")
+];
+
+
+// Get the current hour using Day.js
+var hour = dayjs().hour();
+
+// Initialize variables for user input and the hour span associated with it
+var userInput;
+var hourSpan;
+
+// Set up a function to update the date and time
+function updateDateTime() {
+    // Get the current date and time using Day.js
+  const dateTime = dayjs().format('MMMM D, YYYY h:mm:ss A');
+
+    // Get the HTML element and update its content
+ document.getElementById('currentDateTime').innerText = dateTime;
+ 
+   // Set a timer to update the date and time every second
+ setTimeout(updateDateTime, 1000);
+ }
+
+ // Call the updateDateTime function when the page loads
+  document.addEventListener('DOMContentLoaded', updateDateTime);
+  
+
+
+// Initialize the page by retrieving saved data from Local Storage
+function initPage() {
+  const timeSlots = [
+    "09:00 am",
+    "10:00 am",
+    "11:00 am",
+    "12:00 pm",
+    "01:00 pm",
+    "02:00 pm",
+    "03:00 pm",
+    "04:00 pm",
+    "05:00 pm",
+  ];
+
+  timeSlots.forEach((timeSlot, index) => {
+    const value = JSON.parse(localStorage.getItem(timeSlot));
+    hours[index].val(value);
+  });
+}
+
+// Update the background color of each timeblock based on the current time
+function background() {
+  const timeblocks = document.querySelectorAll(".form-control");
+  timeblocks.forEach((timeblock) => {
+    const timeTest = parseInt(timeblock.getAttribute("id"));
+    const currentHour = dayjs().hour();
+    if (currentHour > timeTest) {
+      timeblock.classList.add("past");
+    } else if (currentHour < timeTest) {
+      timeblock.classList.add("future");
+    } else {
+      timeblock.classList.add("present");
+    }
+  });
+}
+
+// Wait for the page to be fully loaded before executing any code
+$(document).ready(function(){
+  initPage()
+  background()
+
+  // Add click event listener to all "Save" buttons
+  $(".saveBtn").on("click", function(){
+    // Retrieve the user input and hour span associated with the clicked button
+    userInput = $(this).siblings(".form-control").val().trim();
+    hourSpan = $(this).siblings(".input-group-prefix").text().trim();
+    // Save the user input to Local Storage
+    localStorage.setItem(hourSpan, JSON.stringify(userInput));
+
+  })
+
+    // Add click event listener to the "Clear Day" button
+  $("#clearSchedule").on("click", function(){
+    localStorage.clear();
+    // Refresh the page to reflect the updated data
+    initPage()
+  }) 
+
+});
